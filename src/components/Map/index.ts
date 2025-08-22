468c99d90886710675494ab814dde3ac2f53cd53
Ashar Amir <ashharamir@gmail.com>
2025-08-23 01:59:58 +0500

    minimize the map file length, and divide into components

---
+++ src/components/Map/index.ts
@@ -0,0 +1,7 @@
+export { MapHeader } from './MapHeader';
+export { MapView } from './MapView';
+export { FavoritesView } from './FavoritesView';
+export { MapControls } from './MapControls';
+export { MapCrosshair } from './MapCrosshair';
+export { SearchModal } from './SearchModal';
+export { AddLocationModal } from './AddLocationModal';
